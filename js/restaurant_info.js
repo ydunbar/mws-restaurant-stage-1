08e6725292b0f4caa8b244b207e225152366b19b
Yolotzin Dunbar <yltzndnbr@gmail.com>
2018-09-29 12:09:22 -0700

    Added meta viewport tag

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoieWR1bmJhciIsImEiOiJjam1uMW52bTcwN3UxM3JsdXlzNTd3aTY0In0.XD4B8c2IOUIAPG-zkkHUPQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
