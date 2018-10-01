5b75e1758b409cbc3d9077dfcddd808adb78635b
Yolotzin Dunbar <yltzndnbr@gmail.com>
2018-10-01 10:31:49 -0700

    Added aria labels to nav and breadcrumb, and added alt text for each image

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.setAttribute("alt", `${restaurant.name} (${restaurant.neighborhood})`);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
